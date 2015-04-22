cbe9e20eabb4c06092bc3c32e7f31e84c5e60e71
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2015-04-22 12:26:38 +0200

    extracted clean_empty_state

--- cleanuntranslated.py
+++ cleanuntranslated.py
@@ -12,18 +12,21 @@ def segmenthas_states(element, states):
     return any(state in element.attrib.values() for state in states) or 'state' not in element.attrib.keys()
 
 
-def main():
-
+def clean_empty_state(filename):
     NAMESPACE = '{urn:oasis:names:tc:xliff:document:1.2}'
-    source_document = read_xliff_file('sample/fmserver_es_2003.ttk.xlf')
+    source_document = read_xliff_file(filename)
     for target_segment in source_document.iter(tag='{}target'.format(NAMESPACE)):
         if segmenthas_states(target_segment, [None, ""]):
             target_segment.clear()
 
     newtree = ET.ElementTree(source_document)
-    newtree.write('sample/cleaned.xlf',
+    newtree.write("{}.clean".format(filename),
                   encoding='UTF-16',
                   xml_declaration=True)
 
+
+def main():
+    clean_empty_state('./sample/fmserver_es_2003.ttk.xlf')
+    
 if __name__ == "__main__":
     main()
