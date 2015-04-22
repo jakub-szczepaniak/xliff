e82c4aa8147f856cadfe1c8e7979cfa8a281135d
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2015-04-22 17:04:11 +0200

    start implementation of splitting segments for review/untrans

--- cleanuntranslated.py
+++ cleanuntranslated.py
@@ -1,4 +1,6 @@
 from lxml import etree as ET
+from copy import deepcopy
+NAMESPACE = '{urn:oasis:names:tc:xliff:document:1.2}'
 
 
 def read_xliff_file(filename):
@@ -14,17 +16,36 @@ def new_filename(filename, suffix):
     return ".".join(broken)
 
 
-def segmenthas_states(element, states):
+def segmenthas_any_states(element, states):
+    
     return any(state in element.attrib.values() for state in states) or 'state' not in element.attrib.keys()
 
 
-def clean_empty_state(filename):
-    NAMESPACE = '{urn:oasis:names:tc:xliff:document:1.2}'
+def split_xliff(filename):
+
     source_document = read_xliff_file(filename)
+
+    for trans_unit in source_document.iter(tag='{}trans-unit'.format(NAMESPACE)):
+        print(trans_unit)
+
+
+def clean_empty_state(filename):
+
+    source_document = read_xliff_file(filename)
+    review_document = deepcopy(source_document)
     for target_segment in source_document.iter(tag='{}target'.format(NAMESPACE)):
-        if segmenthas_states(target_segment, [None, ""]):
+        if segmenthas_any_states(target_segment, [None, ""]):
             target_segment.clear()
 
+    for target_segment in review_document.iter(tag='{}target'.format(NAMESPACE)):
+        if not segmenthas_any_states(target_segment, ['needs-review-l10n']):
+            target_segment.getparent().clear()
+
+    for_review_tree = ET.ElementTree(review_document)
+    for_review_tree.write(
+        new_filename(filename, 'for_review'),
+        encoding='UTF-16',
+        xml_declaration=True)
     newtree = ET.ElementTree(source_document)
     newtree.write(new_filename(filename, 'clean'),
                   encoding='UTF-16',
@@ -32,7 +53,7 @@ def clean_empty_state(filename):
 
 
 def main():
-    clean_empty_state('./sample/fmserver_es_2003.ttk.xlf')
+    clean_empty_state('./sample/fmserver_es_GUI.ttk.xlf')
 
 if __name__ == "__main__":
     main()
