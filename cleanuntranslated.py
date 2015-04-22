39f5354a908874efab6270ddcfe8e5e2a760f447
jakub-szczepaniak <jakub.szczepaniak@milengo.com>
2015-04-22 12:18:24 +0200

    extracted read_xliff_file and segmenthas_state functions

--- cleanuntranslated.py
+++ cleanuntranslated.py
@@ -1,42 +1,29 @@
-import xml.etree.ElementTree as ET
-import codecs
-from copy import deepcopy
-import re
-
-ET.register_namespace('', 'urn:oasis:names:tc:xliff:document:1.2')
-
-xliff = ET.parse('sample/MAR_doc_user_CZ.xlf')
-xliff_root = xliff.getroot()
-
-NAMESPACE = '{urn:oasis:names:tc:xliff:document:1.2}'
+from lxml import etree as ET
 
 
-def remove_segment_tag(segment):
-    segment = re.sub(r'<seg-source.+?>', '', segment)
-    return re.sub(r'</seg-source>', '', segment)
+def read_xliff_file(filename):
 
-print(xliff_root)
-for segment in xliff_root.iter(tag='{}trans-unit'.format(NAMESPACE)):
-    source = segment[0]
-    target = segment[1]
-    if target.attrib == {}:
-        target.clear()
-        segsource = deepcopy(source)
-        segsource.tag = 'seg-source'
-        segment.append(segsource)
-        as_string = ET.tostring(segsource, encoding='unicode')
-        just_segment = remove_segment_tag(as_string)
-        subsegments = just_segment.split('.')
-        print(len(subsegments))
+    xliff = ET.parse(filename)
+    xliff_root = xliff.getroot()
+    return xliff_root
 
-print(xliff_root)
 
-newtree = ET.ElementTree(xliff_root)
-with codecs.open('test_cleaned.xlf', 'w', encoding='UTF-8') as result:
-    newtree.write(result, encoding='unicode',  xml_declaration=True)
+def segmenthas_states(element, states):
+    return any(state in element.attrib.values() for state in states) or 'state' not in element.attrib.keys()
 
-source_to_segment = '''<ph id="20">&lt;MadCap:variable name="MyVariables.AA"/&gt;</ph>
- for Outlook<bpt id="0">&lt;sup&gt;</bpt>®<ept id="0">
- &lt;/sup&gt;</ept> 2003.'''
-subsegments = source_to_segment.split('.')
-print(subsegments)
+
+def main():
+
+    NAMESPACE = '{urn:oasis:names:tc:xliff:document:1.2}'
+    source_document = read_xliff_file('sample/fmserver_es_2003.ttk.xlf')
+    for target_segment in source_document.iter(tag='{}target'.format(NAMESPACE)):
+        if segmenthas_states(target_segment, [None, ""]):
+            target_segment.clear()
+
+    newtree = ET.ElementTree(source_document)
+    newtree.write('sample/cleaned.xlf',
+                  encoding='UTF-16',
+                  xml_declaration=True)
+
+if __name__ == "__main__":
+    main()
